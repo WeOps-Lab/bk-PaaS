2a7263e20f7d6702df4e1ac170f8730c72305bc3
zhongming Fan <zhongmingfan@canway.net>
2024-05-16 13:42:04 +0800

    feat: get_msg_type接口区分im请求

--- paas2/esb/components/generic/templates/cmsi/get_msg_type.py
+++ paas2/esb/components/generic/templates/cmsi/get_msg_type.py
@@ -9,11 +9,17 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+import json
+
+from common.django_utils import JsonResponse
 
 from components.component import Component, SetupConfMixin
 from common.constants import API_TYPE_Q, HTTP_METHOD
 from common.base_utils import str_bool
+from esb.bkcore.models import ComponentSystem, ESBChannel
+from components.generic.templates.cmsi.toolkit import configs as msg_type_config
 from .toolkit import configs
+from .toolkit.tools import get_base64_icon
 
 
 class GetMsgType(Component, SetupConfMixin):
@@ -42,6 +48,28 @@ class GetMsgType(Component, SetupConfMixin):
                     **({"is_builtin": mt["is_builtin"]} if "is_builtin" in mt else ''),
                 }
             )
+
+        if self.request.kwargs.get("im_channel"):
+            # 补充IM消息通道字段
+            system_id = ComponentSystem.objects.get(name=msg_type_config.SYSTEM_NAME).id
+            channels = ESBChannel.objects.filter_channels([system_id], is_hidden=None, is_active=None)
+
+            for channel in channels:
+                if channel.extra_info:
+                    mapping = json.loads(channel.extra_info)
+                    if mapping.get("type"):
+                        msg_type.append(
+                            {
+                                "name": channel.name,
+                                "type": mapping.get("type", ""),
+                                "icon": get_base64_icon("icons_v2/default_active.ico") if channel.is_active == 1 else get_base64_icon("icons_v2/default_unactive.ico"),
+                                "label": mapping.get("label_en", mapping.get("type", "")) if bk_language == "en" else mapping.get["label"],
+                                "is_active": "true" if channel.is_active == 1 else "false",
+                                "path": channel.path,
+                                "method":  mapping.get("method", "POST"),
+                                "is_builtin": mapping.get("is_builtin", "true"),
+                            }
+                        )
 
         self.response.payload = {
             "result": True,
